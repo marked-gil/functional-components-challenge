e74ad4536177756734fdc0e2e1378a676c1f042f
marked-gil <markgil_creates@yahoo.com>
2023-01-29 12:27:14 +0000

    Create Functional Component Sidebar

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Sidebar() {
+    return (
+        <div>
+            <a href="">My Photos</a>
+            <a href="">My Illustrations</a>
+            <a href="">My Paintings</a>
+        </div>
+    )
+}
+
+export default Sidebar;
