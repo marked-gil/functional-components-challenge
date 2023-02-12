e6b0595872f369723a6584993525a4fb70b52735
marked-gil <markgil_creates@yahoo.com>
2023-02-12 13:50:08 +0000

    Add Loader

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react'
 import css from './css/Content.module.css'
 import {savedPosts} from '../posts.json'
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 class Content extends Component {
     constructor(props) {
       super(props)
       this.state = {
+        isLoaded: false
       }
     }
+
+    componentDidMount() {
+        console.log('It did mount!')
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true
+            })
+        }, 2000)
+    }
+    
     render() {
         return (
         <div>
@@ -16,7 +28,11 @@ class Content extends Component {
                 <h1>My Photos</h1>
             </div>
             <div className={css.SearchResults}>
-                <PostItem savedPosts={savedPosts}/>
+                {
+                    this.state.isLoaded ?
+                    <PostItem savedPosts={savedPosts}/>
+                    : <Loader />
+                }
             </div>
         </div>
         )
